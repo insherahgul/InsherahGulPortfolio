5c2bc3548e197fb55d1e794ff8f24952008018d6
insherahgul <93721204+insherahgul@users.noreply.github.com>
2025-06-26 15:51:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings: ["frontend Devoloper", "WordPress Devoloper", "UI/Ux Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
